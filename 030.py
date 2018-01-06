94f0ee5b60b9d13e514ed62772b10412558a0284
Dan Park <danieljinwoopark@gmail.com>
2018-01-06 08:02:27 -0600

    ex30

--- 030.py
+++ 030.py
@@ -5,7 +5,7 @@ What is the first term in the Fibonacci sequence to contain 1000 digits?
 import math
 
 def main():
-    return(sum([i for i in range(2,9**5*6+1) if i == sum([int(j)**5 for j in str(i)])]))
+    print(sum([i for i in range(2,9**5*6+1) if i == sum([int(j)**5 for j in str(i)])]))
 
 
 if __name__ == "__main__":
